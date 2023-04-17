30382f019f84e493ba1e234848cdd9d9261aeb95
AkashChakrabortty <akashchakrabortty2000@gmail.com>
2023-04-17 13:02:47 +0600

    health section

--- controllers/healthSection.controller.js
+++ controllers/healthSection.controller.js
@@ -1,6 +1,34 @@
-module.exports.getHealthSection =(req,res)=>{
-    res.send('healthSection get found')
+const { updateHealthSectionService, getHealthSectionService } = require("../services/healthSection.service");
+
+module.exports.getHealthSection = async(req,res)=>{
+    try{
+        const result = await getHealthSectionService();
+        res.status(200).json({
+            status: 'success',
+            message: 'Wow! successfully update your health section.',
+            data: result
+        })
+    } 
+    catch(error) {
+        res.status(400).json({ 
+            status: 'error',
+            message: 'Oh sorry! we are not updated your data.',
+        })
+    }
 }
-module.exports.editHealthSection = (req,res)=>{
-    res.send('healthSection post found')
+module.exports.editHealthSection = async(req,res)=>{
+    try{
+        const result = await updateHealthSectionService(req.body);
+        res.status(200).json({
+            status: 'success',
+            message: 'Wow! successfully update your Health section.',
+            data: result
+        })
+    } 
+    catch(error) {
+        res.status(400).json({ 
+            status: 'error',
+            message: 'Oh sorry! we are not updated your data.',
+        })
+    }
 }
