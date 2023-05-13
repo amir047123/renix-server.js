257eef47d519c12c15cfd92030bc7055407dd6be
Jillu Rahman Jibon <jillurahmanjibon@gmail.com>
2023-05-13 14:54:55 +0600

    env file update

--- server.js
+++ server.js
@@ -1,27 +1,34 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const dotenv = require("dotenv").config();
 const colors = require("colors");
-
 const app = require("./app");
+
+// server
 const port = process.env.PORT || 8080;
 
-const start = async () => {
-  try {
 
-    await mongoose.connect(process.env.DATABASE, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+// database connection
+mongoose.connect(process.env.DATABASE).then(() => {
+  console.log(`Database connection is successful`.red.italic.bold);
+});
+mongoose.connect('mongodb+srv://organicherbalofficial:ZbbdRYIHnD6WihCT@cluster0.snumnte.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error(error));
+const uri = process.env.DATABASE;
 
-    app.listen(port, () => {
-      console.log(`server is running on ${port}`)
-    });
-  }
-  catch (err) {
-    console.log(err)
-  }
-}
-
-start();
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 
+
+
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`.yellow.italic.bold);
+});
