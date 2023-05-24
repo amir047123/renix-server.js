787d122a9eeb9295335643d1768886c9bf87a363
ifty03 <ashikulislamb84@gmail.com>
2023-05-24 17:10:54 +0600

    register adde

---
+++ models/user.model.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+    },
+
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+
+    confirmPassword: {
+      type: String,
+      required: [true, "Please confirm your password"],
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin", "doctor"],
+      default: "user",
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    fullName: {
+      type: String,
+      required: [true, "Please provide your Full Name"],
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", function (next) {
+  const password = this.password;
+  const hashedPassword = bcrypt.hashSync(password);
+  this.password = hashedPassword;
+  this.confirmPassword = undefined;
+  next();
+});
+
+userSchema.methods.comparePassword = function (password, hash) {
+  const isPasswordValid = bcrypt.compareSync(password, hash);
+  return isPasswordValid;
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
---
+++ routes/user.route.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/user.controller");
+
+router.get("/", userController.getUsers);
+router.post("/signup", userController.singUp);
+router.patch("/verify", userController.verifyEmail);
+router.post("/login", userController.logIn);
+router.get("/me", userController.getMe);
+router.post("/forgot-password", userController.forgotPassword);
+router.route("/by-email").get(userController.getUserByQuery);
+router.post("/delete-ip/:id", userController.deleteUserIp);
+router.get("/get-user-ip", userController.getUserIp);
+
+router
+  .route("/:id")
+  .get(userController.getUserById)
+  .delete(userController.deleteUser)
+  .patch(userController.updateUser);
+
+module.exports = router;
