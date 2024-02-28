0bdf28226972765d71a67dd50aa50fe6c36862ea
Reshma62 <mernstack2024@gmail.com>
2024-02-29 05:50:24 +0600

    aded

--- models/category.model.js
+++ models/category.model.js
@@ -1,7 +1,28 @@
 const mongoose = require("mongoose");
 
 const categorySchema = mongoose.Schema({
-  name: { type: String, required: [true, "Please provide category"] },
+  name: {
+    type: String,
+    required: [true, "Please provide category"],
+  },
+  // meta seo
+  metaTitle: {
+    type: String,
+  },
+  metaDescription: {
+    type: String,
+  },
+  metaImage: {
+    type: String,
+  },
+  canonicalUrl: {
+    type: String,
+  },
+  slug: {
+    type: String,
+    required: [true, "Please provide category"],
+    unique: true,
+  },
 });
 
 const Category = mongoose.model("Category", categorySchema);
